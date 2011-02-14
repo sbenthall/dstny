95b31b9bc447112807f66434819019f2d461f0f6
Sebastian Benthall <sbenthall@gmail.com>
2011-02-14 17:29:50 -0500

    metadata and edge adding UI bits

--- static/dstny.js
+++ static/dstny.js
@@ -20,9 +20,7 @@ $(document).ready(function(){
             var nodeid = $(this).attr('data-node');
             $.ajax('/node/' + nodeid, {
                 type: 'DELETE',
-                data: {
-
-                },
+                data: {},
                 dataType: "json",
                 context: this,
                 success: function(data){
@@ -32,6 +30,63 @@ $(document).ready(function(){
         }
     );
 
+    $('.remove-edge-span').click(
+        function(evt){
+            edge_url = '/edge/'+ nodeid + '/'
+                + $(this).attr('data-node');
+            $.ajax(edge_url, {
+                type: 'DELETE',
+                data: {},
+                dataType: "json",
+                context: this,
+                success: function(data){
+                    $(this).parent().remove()
+                }
+            })
+        }
+    );
+
+    $('#add-edge-button').click(
+        function(evt){
+            edge_url = '/edge/'+ nodeid + '/'
+                + $('#to-node-input').val();
+            $.ajax(edge_url, {
+                type: 'PUT',
+                data: {},
+                success: function(){
+                    location.reload();
+                }
+            })
+        });
+
+
+    $('#add-metadata-key-button').click(
+        function(evt){
+            window.location = window.location + "/metadata/"
+                + $("#metadata-key-input").val();
+        });
+
+    $('#enter-metadata-value-button').click(
+        function(evt){
+            metadata_url = "/node/" + nodeid + "/metadata/" + key;
+            $.ajax(metadata_url,
+                   {
+                       type: 'POST',
+                       data: {
+                           'value' : $('#enter-metadata-value-textarea').val()
+                       },
+                       success: function(){
+                           window.location = "/node/" + nodeid;
+                       }
+                   })
+        }
+    );
+
+    $("#edit-metadata-span").click(
+        function(evt){
+            window.location = window.location + "/metadata/"
+                + $(this).attr("data-key") + "?edit";
+        });
 
 })
   
